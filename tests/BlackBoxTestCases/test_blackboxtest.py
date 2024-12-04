7a57797d99c4e46a1fdcfefc2496971fc89c7bdf
Superman9999 <agnelonunes43@gmail.com>
2024-12-04 12:28:30 -0500

    Update test_blackboxtest.py

--- tests/BlackBoxTestCases/test_blackboxtest.py
+++ tests/BlackBoxTestCases/test_blackboxtest.py
@@ -15,9 +15,10 @@ def test_website_health_endpoint2():
     
     #Check the status code of '/resume-upload' endpoint
     assert response.status_code == 200
-
+'''
 def test_backend_endpoint():
     form_data = {'resume':"this is a sample resume"}
     response = requests.post('https://relevance-backend-api-338628837206.us-central1.run.app/api/getPrediction', form_data)
 
     assert response.status_code == 200
+    '''
